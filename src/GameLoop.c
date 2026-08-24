3f0e0ddaa56151f55534fb0d6c21b66cc765d7f5
floatghost <levin.liechti@hotmail.com>
2026-08-24 10:34:09 +0200

    sigma

--- src/GameLoop.c
+++ src/GameLoop.c
@@ -5,10 +5,13 @@
 #include "include/GameLoop.h"
 #include "raylib.h"
 void GameLoop() {
+    Texture2D texture_2d = LoadTexture("../assets/zero_walahis_left.png");
+
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(BLACK);
         DrawText("Hello",10,10,10,RAYWHITE);
+        DrawTexture(texture_2d, 0, 0, WHITE);
         EndDrawing();
     }
 }
